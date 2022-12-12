6b3cece976518c7a9c7354131f20d35744cbc3b5
Mary <chin95.mit@gmail.com>
2022-12-12 17:28:07 +0800

    added userController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\JobOrderController;
 use App\Http\Controllers\WorkLogController;
 use App\Http\Controllers\RolesController;
@@ -18,13 +19,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('joborder/search', [JobOrderController::class, 'search']);
-Route::resource('joborder', JobOrderController::class);
-Route::resource('worklog', WorkLogController::class);
-Route::resource('workers', WorkersController::class);
-Route::resource('roles', RolesController::class);
-Route::resource('orderdetails', JobOrderDetailController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::get('joborder/search', [JobOrderController::class, 'search']);
+    Route::resource('joborder', JobOrderController::class);
+    Route::resource('worklog', WorkLogController::class);
+    Route::resource('workers', WorkersController::class);
+    Route::resource('roles', RolesController::class);
+    Route::resource('orderdetails', JobOrderDetailController::class);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
